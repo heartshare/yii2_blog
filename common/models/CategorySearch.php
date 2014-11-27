e8bcc4cc19db926eb83cc8e8d8701d319453a761
Mole <niefufeng@gmail.com>
2014-11-27 22:31:51 +0800

    初始化分类搜索模型

---
+++ common/models/CategorySearch.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Category;
+
+/**
+ * CategorySearch represents the model behind the search form about `common\models\Category`.
+ */
+class CategorySearch extends Category
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'sort', 'parent_id'], 'integer'],
+            [['category_name', 'slug', 'description'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Category::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'sort' => $this->sort,
+            'parent_id' => $this->parent_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'category_name', $this->category_name])
+            ->andFilterWhere(['like', 'slug', $this->slug])
+            ->andFilterWhere(['like', 'description', $this->description]);
+
+        return $dataProvider;
+    }
+}
