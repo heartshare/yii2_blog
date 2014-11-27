11ad8be267d733fc6fc8274132754bf9cd03ebb2
Mole <niefufeng@gmail.com>
2014-11-27 22:32:06 +0800

    初始化用户搜索模型

---
+++ common/models/UserSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\User;
+
+/**
+ * UserSearch represents the model behind the search form about `common\models\User`.
+ */
+class UserSearch extends User
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'gender', 'phone', 'create_time', 'update_time', 'active_time', 'status', 'role_id'], 'integer'],
+            [['username', 'nickname', 'email', 'password_hash', 'profile', 'avatar', 'auth_key', 'password_reset_token', 'site'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = User::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'gender' => $this->gender,
+            'phone' => $this->phone,
+            'create_time' => $this->create_time,
+            'update_time' => $this->update_time,
+            'active_time' => $this->active_time,
+            'status' => $this->status,
+            'role_id' => $this->role_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'username', $this->username])
+            ->andFilterWhere(['like', 'nickname', $this->nickname])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'password_hash', $this->password_hash])
+            ->andFilterWhere(['like', 'profile', $this->profile])
+            ->andFilterWhere(['like', 'auth_key', $this->auth_key])
+            ->andFilterWhere(['like', 'password_reset_token', $this->password_reset_token])
+            ->andFilterWhere(['like', 'site', $this->site]);
+
+        return $dataProvider;
+    }
+}
