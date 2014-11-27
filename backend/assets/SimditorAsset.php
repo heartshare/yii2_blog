047a1f571caca037bc896827a0952ccb381a4a7e
Mole <niefufeng@gmail.com>
2014-11-27 22:29:44 +0800

    +Simditor编辑器Asset文件

---
+++ backend/assets/SimditorAsset.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Simditor富文本编辑器
+ */
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Fufeng Nie <niefufeng@gmail.com>
+ * @since 2.0
+ */
+class SimditorAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'plugins/simditor/styles/font-awesome.css',
+        'plugins/simditor/styles/simditor.css'
+    ];
+    public $js = [
+        'plugins/simditor/scripts/module.min.js',
+        'plugins/simditor/scripts/uploader.min.js',
+        'plugins/simditor/scripts/simditor.min.js',
+        'js/simditor-common.js'
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset'
+    ];
+}
