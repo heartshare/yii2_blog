a860356374ffa958d96c94ea3e14596aeefe491c
Mole <niefufeng@gmail.com>
2014-11-27 22:42:40 +0800

    初始化文章评论搜索模型

---
+++ common/models/ArticleCommentsSearch.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\ArticleComments;
+
+/**
+ * ArticleCommentsSearch represents the model behind the search form about `common\models\ArticleComments`.
+ */
+class ArticleCommentsSearch extends ArticleComments
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'create_time', 'reply_to', 'parent_id', 'status', 'article_id', 'user_id'], 'integer'],
+            [['content', 'ip', 'agent'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = ArticleComments::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        if (!($this->load($params) && $this->validate())) {
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'create_time' => $this->create_time,
+            'reply_to' => $this->reply_to,
+            'parent_id' => $this->parent_id,
+            'status' => $this->status,
+            'article_id' => $this->article_id,
+            'user_id' => $this->user_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'content', $this->content])
+            ->andFilterWhere(['like', 'ip', $this->ip])
+            ->andFilterWhere(['like', 'agent', $this->agent]);
+
+        return $dataProvider;
+    }
+}
