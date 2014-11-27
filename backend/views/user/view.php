0be222d2a36ec044d1af45fc3011a05dd6d20031
Mole <niefufeng@gmail.com>
2014-11-27 22:37:53 +0800

    初始化用户详情页

---
+++ backend/views/user/view.php
@@ -0,0 +1,51 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\User */
+
+$this->title = $model->nickname.'\'s Detail';
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Users'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="user-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'username',
+            'nickname',
+            'email:email',
+            'gender',
+            'phone',
+            'password_hash',
+            'profile',
+            'avatar',
+            'create_time',
+            'update_time',
+            'active_time',
+            'status',
+            'auth_key',
+            'password_reset_token',
+            'site',
+            'role_id',
+        ],
+    ]) ?>
+
+</div>
