776f41aff59d3e9892ee453ed8f023ed6e1ea9af
Mole <niefufeng@gmail.com>
2014-11-27 22:27:41 +0800

    完善了模型

--- common/models/User.php
+++ common/models/User.php
@@ -5,28 +5,48 @@ use Yii;
 use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
+use yii\rbac\Role;
 use yii\web\IdentityInterface;
 
 /**
- * User model
+ * This is the model class for table "{{%user}}".
  *
- * @property integer $id
+ * @property string $id
  * @property string $username
- * @property string $password_hash
- * @property string $password_reset_token
+ * @property string $nickname
  * @property string $email
- * @property string $auth_key
- * @property integer $role
+ * @property integer $gender
+ * @property string $phone
+ * @property string $password_hash
+ * @property string $profile
+ * @property string $avatar
+ * @property string $create_time
+ * @property string $update_time
+ * @property string $active_time
  * @property integer $status
- * @property integer $created_at
- * @property integer $updated_at
- * @property string $password write-only password
+ * @property string $auth_key
+ * @property string $password_reset_token
+ * @property string $site
+ * @property string $role_id
+ *
+ * @property string $password
+ *
+ * @property Album[] $albums
+ * @property Article[] $articles
+ * @property ArticleComments[] $articleComments
  */
 class User extends ActiveRecord implements IdentityInterface
 {
     const STATUS_DELETED = 0;
-    const STATUS_ACTIVE = 10;
-    const ROLE_USER = 10;
+    const STATUS_ACTIVE = 1;
+    const STATUS_LUCK = 2;
+    const STATUS_VERIFY = 3;
+    const ROLE_USER = 2;
+    const ROLE_ADMIN = 1;
+    const GENDER_GIRL = 0;
+    const GENDER_BOY = 1;
+
+    public $password;
 
     /**
      * @inheritdoc
@@ -42,7 +62,13 @@ class User extends ActiveRecord implements IdentityInterface
     public function behaviors()
     {
         return [
-            TimestampBehavior::className(),
+            'timestamp' => [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['create_time', 'active_time', 'update_time'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'update_time',
+                ],
+            ]
         ];
     }
 
@@ -52,11 +78,91 @@ class User extends ActiveRecord implements IdentityInterface
     public function rules()
     {
         return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+            [['gender', 'phone', 'create_time', 'update_time', 'active_time', 'status', 'role_id'], 'integer'],
+            [['username', 'nickname'], 'string', 'max' => 45],
+            [['email', 'password_hash', 'profile', 'site'], 'string', 'max' => 255],
+            [['avatar', 'auth_key', 'password_reset_token'], 'string', 'max' => 100],
+            [['username'], 'unique'],
+            [['nickname'], 'unique'],
+            [['email'], 'unique'],
+            ['status', 'default', 'value' => self::STATUS_VERIFY],
+            [
+                'status',
+                'in',
+                'range' => [
+                    self::STATUS_ACTIVE,
+                    self::STATUS_DELETED,
+                    self::STATUS_LUCK,
+                    self::STATUS_VERIFY
+                ]
+            ],
+            ['gender', 'default', 'value' => self::GENDER_BOY],
+            ['password', 'safe']
+        ];
+    }
 
-            ['role', 'default', 'value' => self::ROLE_USER],
-            ['role', 'in', 'range' => [self::ROLE_USER]],
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'username' => Yii::t('app', 'Username'),
+            'nickname' => Yii::t('app', 'Nickname'),
+            'email' => Yii::t('app', 'Email'),
+            'gender' => Yii::t('app', 'Gender'),
+            'phone' => Yii::t('app', 'Phone'),
+            'password_hash' => Yii::t('app', 'Password Hash'),
+            'profile' => Yii::t('app', 'Profile'),
+            'avatar' => Yii::t('app', 'avatar'),
+            'create_time' => Yii::t('app', 'Create Time'),
+            'update_time' => Yii::t('app', 'Update Time'),
+            'active_time' => Yii::t('app', 'Active Time'),
+            'status' => Yii::t('app', 'Status'),
+            'auth_key' => Yii::t('app', 'Auth Key'),
+            'password_reset_token' => Yii::t('app', 'Password Reset Token'),
+            'site' => Yii::t('app', 'Site'),
+            'role_id' => Yii::t('app', 'Role ID'),
+            'password' => Yii::t('app', 'password')
+        ];
+    }
+
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if (!empty($this->password)) {
+                $this->setPassword($this->password);
+            }
+            if (empty($this->nickname)) {
+                $this->nickname = $this->username;
+            }
+            if (empty($this->role_id)) {
+                //TODO Yii2的RBAC还不会，所以先随机读取一个^_^
+                $this->role_id = \common\models\Role::find()->one()->id;
+            }
+            if ($this->isNewRecord) {
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public function getStatusOptions()
+    {
+        return [
+            self::STATUS_ACTIVE => Yii::t('app', 'Active'),
+            self::STATUS_DELETED => Yii::t('app', 'Delete'),
+            self::STATUS_LUCK => Yii::t('app', 'Luck'),
+            self::STATUS_VERIFY => Yii::t('app', 'Verify')
+        ];
+    }
+
+    public function getGenderOptions()
+    {
+        return [
+            self::GENDER_GIRL => Yii::t('app', 'girl'),
+            self::GENDER_BOY => Yii::t('app', 'boy')
         ];
     }
 
@@ -118,7 +224,7 @@ class User extends ActiveRecord implements IdentityInterface
         }
         $expire = Yii::$app->params['user.passwordResetTokenExpire'];
         $parts = explode('_', $token);
-        $timestamp = (int) end($parts);
+        $timestamp = (int)end($parts);
         return $timestamp + $expire >= time();
     }
 
@@ -189,5 +295,34 @@ class User extends ActiveRecord implements IdentityInterface
     public function removePasswordResetToken()
     {
         $this->password_reset_token = null;
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAlbums()
+    {
+        return $this->hasMany(Album::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticles()
+    {
+        return $this->hasMany(Article::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getArticleComments()
+    {
+        return $this->hasMany(ArticleComments::className(), ['user_id' => 'id']);
+    }
+
+    public function getRoles()
+    {
+        return $this->hasOne(Role::className(), ['id' => 'role_id']);
     }
 }
