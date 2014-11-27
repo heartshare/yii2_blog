794f250410d6abb3c54bd9fa27ac1924b03a5d06
Mole <niefufeng@gmail.com>
2014-11-27 22:33:55 +0800

    初始化文章搜索页面

---
+++ backend/views/article/_search.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\ArticleSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="article-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'content') ?>
+
+    <?= $form->field($model, 'create_time') ?>
+
+    <?= $form->field($model, 'modify_time') ?>
+
+    <?php // echo $form->field($model, 'type') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <?php // echo $form->field($model, 'top') ?>
+
+    <?php // echo $form->field($model, 'view') ?>
+
+    <?php // echo $form->field($model, 'sort') ?>
+
+    <?php // echo $form->field($model, 'slug') ?>
+
+    <?php // echo $form->field($model, 'excerpt') ?>
+
+    <?php // echo $form->field($model, 'password') ?>
+
+    <?php // echo $form->field($model, 'allow_comment') ?>
+
+    <?php // echo $form->field($model, 'comments_total') ?>
+
+    <?php // echo $form->field($model, 'user_id') ?>
+
+    <?php // echo $form->field($model, 'category_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
