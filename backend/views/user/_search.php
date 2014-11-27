0f75eca2097f2d7cffcd46f5ed6c8857fdd467e4
Mole <niefufeng@gmail.com>
2014-11-27 22:37:37 +0800

    初始化用户搜索页

---
+++ backend/views/user/_search.php
@@ -0,0 +1,59 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\UserSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'username') ?>
+
+    <?= $form->field($model, 'nickname') ?>
+
+    <?= $form->field($model, 'email') ?>
+
+    <?= $form->field($model, 'gender') ?>
+
+    <?php // echo $form->field($model, 'phone') ?>
+
+    <?php // echo $form->field($model, 'password_hash') ?>
+
+    <?php // echo $form->field($model, 'profile') ?>
+
+    <?php // echo $form->field($model, 'head_picture') ?>
+
+    <?php // echo $form->field($model, 'create_time') ?>
+
+    <?php // echo $form->field($model, 'update_time') ?>
+
+    <?php // echo $form->field($model, 'active_time') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <?php // echo $form->field($model, 'auth_key') ?>
+
+    <?php // echo $form->field($model, 'password_reset_token') ?>
+
+    <?php // echo $form->field($model, 'site') ?>
+
+    <?php // echo $form->field($model, 'role_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
