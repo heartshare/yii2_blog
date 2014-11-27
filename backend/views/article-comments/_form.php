3b29500745c2317c2929afd2ae3acaeacbdb68d1
Mole <niefufeng@gmail.com>
2014-11-27 22:43:26 +0800

    初始化文章评论表单

---
+++ backend/views/article-comments/_form.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\ArticleComments */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="article-comments-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'content')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'create_time')->textInput(['maxlength' => 10]) ?>
+
+    <?= $form->field($model, 'ip')->textInput(['maxlength' => 100]) ?>
+
+    <?= $form->field($model, 'agent')->textInput(['maxlength' => 255]) ?>
+
+    <?= $form->field($model, 'reply_to')->textInput(['maxlength' => 10]) ?>
+
+    <?= $form->field($model, 'parent_id')->textInput(['maxlength' => 20]) ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <?= $form->field($model, 'article_id')->textInput(['maxlength' => 10]) ?>
+
+    <?= $form->field($model, 'user_id')->textInput(['maxlength' => 10]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
