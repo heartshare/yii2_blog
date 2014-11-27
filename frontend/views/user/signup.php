1e03cff951223e169e85fc4fd6c15498361a491d
Mole <niefufeng@gmail.com>
2014-11-27 22:32:38 +0800

    增加前台注册页面

---
+++ frontend/views/user/signup.php
@@ -0,0 +1,49 @@
+<?php
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\SignupForm */
+/* @var $form ActiveForm */
+
+$this->title = 'Signup';
+?>
+<div class="user-signup">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>Please fill out the following fields to signup:</p>
+
+    <div class="row">
+        <div class="col-lg-6">
+            <?php $form = ActiveForm::begin([
+                'layout' => 'horizontal',
+                'fieldConfig' => [
+                    'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+                    'horizontalCssClasses' => [
+                        'label' => 'col-sm-3',
+                        'offset' => 'col-sm-offset-3',
+                        'wrapper' => 'col-sm-9',
+                        'error' => '',
+                        'hint' => '',
+                    ],
+                ],
+            ]); ?>
+
+            <?= $form->field($model, 'username') ?>
+
+            <?= $form->field($model, 'email') ?>
+
+            <?= $form->field($model, 'password')->passwordInput() ?>
+
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-9">
+                    <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
+                </div>
+            </div>
+            <?php ActiveForm::end(); ?>
+
+        </div>
+    </div>
+</div>
+<!-- user-signup -->
