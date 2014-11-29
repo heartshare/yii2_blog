7244d198ef95ce87d71a71eb2c922e9805b8496e
Mole <niefufeng@gmail.com>
2014-11-30 00:05:01 +0800

    统一继承了CommonController类并删除了当前类的behaviors方法

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -1,46 +1,15 @@
 <?php
 namespace backend\controllers;
 
-use Yii;
-use yii\filters\AccessControl;
-use yii\web\Controller;
 use common\models\LoginForm;
-use yii\filters\VerbFilter;
+use backend\controllers\CommonController;
 
 /**
  * Site controller
+ * @package backend\controllers
  */
-class SiteController extends Controller
+class SiteController extends CommonController
 {
-    /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'actions' => ['login', 'error'],
-                        'allow' => true,
-                    ],
-                    [
-                        'actions' => ['logout', 'index'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
     /**
      * @inheritdoc
      */
