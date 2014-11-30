72241ce9211261b1568837440a8df014e9d21d38
Mole <niefufeng@gmail.com>
2014-11-30 23:43:23 +0800

    删除了角色相关代码；编辑器类名改为id

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -4,13 +4,11 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use backend\assets\SimditorAsset;
 use yii\helpers\ArrayHelper;
-use common\models\Role;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 /* @var $form yii\bootstrap\ActiveForm */
 SimditorAsset::register($this);
-$roles = ArrayHelper::map(Role::find()->asArray()->all(),'id','name');
 ?>
 
 <div class="user-form">
@@ -45,11 +43,9 @@ $roles = ArrayHelper::map(Role::find()->asArray()->all(),'id','name');
 
     <?= $form->field($model,'avatar')->fileInput() ?>
 
-    <?= $form->field($model,'profile')->textarea(['class'=>'editor']) ?>
+    <?= $form->field($model,'profile')->textarea(['id'=>'editor']) ?>
 
     <?= $form->field($model,'site')->textInput() ?>
-
-    <?= $form->field($model,'role_id')->dropDownList($roles) ?>
 
     <div class="form-group">
         <div class="col-sm-offset-3 col-sm-9">
