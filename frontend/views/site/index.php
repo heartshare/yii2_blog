6b34744f3b0672e5fe9c8f3f611b51b8ff469574
Mole <niefufeng@gmail.com>
2014-11-30 23:50:48 +0800

    首页显示最新的3篇文章

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,6 +1,8 @@
 <?php
+use yii\helpers\Html;
+use yii\helpers\Url;
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'My Blog';
 ?>
 <div class="site-index">
 
@@ -15,36 +17,15 @@ $this->title = 'My Yii Application';
     <div class="body-content">
 
         <div class="row">
+            <?php foreach($articles as $article): ?>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <h2><a href="<?= Url::toRoute(['article/view', 'id' => $article['id']]) ?>"><?= Html::encode($article['title']) ?></a></h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                <p><?= Html::encode($article['excerpt']) ?></p>
 
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                <p><a class="btn btn-default" href="<?= Url::toRoute(['article/view']) ?>">Read more... &raquo;</a></p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            <?php endforeach; ?>
         </div>
 
     </div>
