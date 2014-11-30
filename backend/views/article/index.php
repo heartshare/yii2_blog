56df56fe593427c74f62a761f8aa9b85292ff22c
Mole <niefufeng@gmail.com>
2014-11-30 23:46:54 +0800

    对时间戳进行了格式化

--- backend/views/article/index.php
+++ backend/views/article/index.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\grid\GridView;
+use yii\grid\DataColumn;
 
 /* @var $this yii\web\View */
 /* @var $searchModel common\models\ArticleSearch */
@@ -17,8 +18,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a(Yii::t('app', 'Create {modelClass}', [
-    'modelClass' => 'Article',
-]), ['create'], ['class' => 'btn btn-success']) ?>
+            'modelClass' => 'Article',
+        ]), ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
@@ -29,9 +30,19 @@ $this->params['breadcrumbs'][] = $this->title;
 
             'id',
             'title',
-            'content:ntext',
-            'create_time',
-            'update_time',
+//            'content:ntext',
+            [
+                'class' => DataColumn::className(),
+                'attribute' => 'create_time',
+                'format' => ['date', 'php:Y-m-d'],
+                'label' => '创建时间'
+            ],
+            [
+                'class' => DataColumn::className(),
+                'attribute' => 'update_time',
+                'format' => ['date', 'php:Y-m-d'],
+                'label' => '更新时间'
+            ],
             // 'type',
             // 'status',
             // 'top',
