0b5cb2d881d6a1ba7718b6f0fb7d7e24e824c57f
Fufeng.Nie <niefufeng@gmail.com>
2014-12-09 16:10:10 +0800

    修改了右侧热门文章的展示和排版效果

--- frontend/views/article/list.php
+++ frontend/views/article/list.php
@@ -51,15 +51,13 @@ $this->params['breadcrumbs'][] = $this->title;
             <div class="panel-heading">
                 <i class="glyphicon glyphicon-fire"></i>&nbsp;热门文章
             </div>
-            <div class="panel-body">
-                <ul class="list-unstyled">
-                    <?php foreach ($hotArticles['list'] as $article): ?>
-                        <li><?= Html::a($article['title'],
-                                ['article/view', 'id' => $article['id']]) ?>
-                        </li>
-                    <?php endforeach; ?>
-                </ul>
-            </div>
+            <ul class="list-group">
+                <?php foreach ($hotArticles['list'] as $article): ?>
+                    <li class="list-group-item"><?= Html::a($article['title'],
+                            ['article/view', 'id' => $article['id']]) ?>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
         </div>
     </div>
 </div>
