1b0c6f78932d6b80ee7699ac7e631619e0cca55b
Fufeng.Nie <niefufeng@gmail.com>
2014-12-10 19:41:53 +0800

    category_name改为了name

--- backend/views/category/update.php
+++ backend/views/category/update.php
@@ -7,9 +7,9 @@ use yii\helpers\Html;
 
 $this->title = Yii::t('app', 'Update {modelClass}: ', [
     'modelClass' => 'Category',
-]) . ' ' . $model->category_name;
+]) . ' ' . $model->name;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Categories'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->category_name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 <div class="category-update">
