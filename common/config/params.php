8ab3b350e11efe102ceb014decc54d992bf3e5fb
Fufeng.Nie <niefufeng@gmail.com>
2014-12-18 09:39:55 +0800

    +发送通知的邮箱，因为国内企业邮箱需要

--- common/config/params.php
+++ common/config/params.php
@@ -2,7 +2,8 @@
 return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
+    'notificationEmail' => 'notification@example.com',
     'user.passwordResetTokenExpire' => 3600,
 
-    'sitename' => '空山鸟语'
+    'sitename' => '网站名称'
 ];
