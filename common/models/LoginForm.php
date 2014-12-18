4ad86de7c3ef24d73a7c70bc290e7f5de8b46dd8
Fufeng.Nie <niefufeng@gmail.com>
2014-12-18 09:52:48 +0800

    +模型属性方法，以便前台登录时能进行翻译

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -30,6 +30,17 @@ class LoginForm extends Model
         ];
     }
 
+    public function attributeLabels()
+    {
+        return [
+            'username' => Yii::t('app', 'Username'),
+            'email' => Yii::t('app', 'Email'),
+            'auth_key' => Yii::t('app', 'Auth Key'),
+            'password' => Yii::t('app', 'Password'),
+            'rememberMe' => Yii::t('app','Remember Me')
+        ];
+    }
+
     /**
      * Validates the password.
      * This method serves as the inline validation for password.
