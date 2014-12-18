8a35086fdf8af505e2c3765d76adf38f7a2bb040
Fufeng.Nie <niefufeng@gmail.com>
2014-12-18 10:04:16 +0800

    修复了使用国内邮箱smtp服务501错误的bug

--- frontend/models/ContactForm.php
+++ frontend/models/ContactForm.php
@@ -37,23 +37,29 @@ class ContactForm extends Model
     public function attributeLabels()
     {
         return [
-            'verifyCode' => 'Verification Code',
+            'verifyCode' => Yii::t('app', 'Verification Code'),
+            'name' => Yii::t('app', 'Name'),
+            'email' => Yii::t('app', 'Email'),
+            'subject' => Yii::t('app', 'Subject'),
+            'body' => Yii::t('app', 'Body')
         ];
     }
 
     /**
      * Sends an email to the specified email address using the information collected by this model.
      *
-     * @param  string  $email the target email address
+     * @param  string $email the target email address
      * @return boolean whether the email was sent
      */
     public function sendEmail($email)
     {
         return Yii::$app->mailer->compose()
             ->setTo($email)
-            ->setFrom([$this->email => $this->name])
+            //setFrom参数的键值可以设置为发件人$this->email的邮箱地址，但是测试只有墙外的邮箱支持(比如Gmail)，国内如网易和腾讯都不支持，会报501错误，并提示不能用虚假邮箱地址。
+            //如果使用国内邮箱发送，填发送邮件的邮箱地址即可。
+            ->setFrom([Yii::$app->params['notificationEmail'] => $this->name])
             ->setSubject($this->subject)
-            ->setTextBody($this->body)
+            ->setTextBody('Form:' . $this->email . "\n" . $this->body)
             ->send();
     }
 }
