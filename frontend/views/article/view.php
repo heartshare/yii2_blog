5db251f7958b951cce1496307c1b3da79e0e7f15
Fufeng.Nie <niefufeng@gmail.com>
2014-12-18 10:09:25 +0800

    对页面一些文本使用了Yii国际化组件

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -14,22 +14,23 @@ $this->params['breadcrumbs'][] = $this->title;
 <div id="article-info" class="row">
     <div class="col-lg-3 col-sm-6">
         <i class="glyphicon glyphicon-calendar"></i>&nbsp;
-        发布于公元<?= date('Y-m-d H:i:s', $article['create_time']) ?>
+        <?= Yii::t('app', 'Posted {time}', ['time' => date('Y-m-d H:i:s', $article['create_time'])]) ?>
     </div>
     <div class="col-lg-3 col-sm-6">
         <i class="glyphicon glyphicon-list"></i>&nbsp;
-        所属分类：<?= Html::a($article['category']['name'], Url::toRoute(['/category/' . $article['category']['slug']])) ?>
+        <?= Yii::t('app', 'Category') ?>
+        ：<?= Html::a($article['category']['name'], Url::toRoute(['/category/' . $article['category']['slug']])) ?>
     </div>
     <div class="col-lg-3 col-sm-6">
         <i class="glyphicon glyphicon-eye-open"></i>&nbsp;
-        看热闹打酱油的有<?= $article['view'] ?>人
+        <?= Yii::t('app', 'Views {total}', ['total' => $article['view']]) ?>
     </div>
     <div class="col-lg-3 col-sm-6">
         <i class="glyphicon glyphicon-comment"></i>&nbsp;
-        <?php if($article['comments_total'] > 0): ?>
-            已有<?= $article['comments_total'] ?>人发表读后感
+        <?php if ($article['comments_total'] > 0): ?>
+            <?= Yii::t('app', 'Comments {total}', ['total' => $article['comments_total']]) ?>
         <?php else: ?>
-            还没人鸟我哟o(╯□╰)o
+            <?= Yii::t('app', 'No Comments') ?>
         <?php endif; ?>
     </div>
 </div>
