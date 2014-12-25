3955ad566f7940d226f8959d4894f43cdee0f028
Fufeng.Nie <niefufeng@gmail.com>
2014-12-25 19:37:51 +0800

    新增评论表单；引入了Simditor编辑器；注释了评论统计

--- frontend/views/article/view.php
+++ frontend/views/article/view.php
@@ -1,40 +1,88 @@
 <?php
 use yii\helpers\Html;
 use yii\helpers\Url;
+use yii\bootstrap\ActiveForm;
+use common\assets\SimditorAsset;
 
 /* @var $this yii\web\View */
 
 $this->title = $article['title'];
 $this->params['breadcrumbs'][] = ['label' => $article['category']['name'], 'url' => ['/category/' . $article['category']['slug']]];
 $this->params['breadcrumbs'][] = $this->title;
+SimditorAsset::register($this);
+
+$form = ActiveForm::begin([
+    'layout' => 'horizontal',
+    'fieldConfig' => [
+        'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
+        'horizontalCssClasses' => [
+            'label' => 'col-sm-2',
+            'offset' => 'col-sm-offset-2',
+            'wrapper' => 'col-sm-10',
+            'error' => '',
+            'hint' => '',
+        ],
+    ],
+]);
+
+$this->registerJs('$("#insert-comment").click(function(){
+            if($.trim($("#editor").val()) == ""){
+                alert("您不说点什么就想提交么？");
+                return false;
+            }
+        })');
 ?>
 
-<h3><?= Html::encode($article['title']) ?></h3>
+    <h3><?= Html::encode($article['title']) ?></h3>
 
-<div id="article-info" class="row">
-    <div class="col-lg-3 col-sm-6">
-        <i class="glyphicon glyphicon-calendar"></i>&nbsp;
-        <?= Yii::t('app', 'Posted {time}', ['time' => date('Y-m-d H:i:s', $article['create_at'])]) ?>
-    </div>
-    <div class="col-lg-3 col-sm-6">
-        <i class="glyphicon glyphicon-list"></i>&nbsp;
-        <?= Yii::t('app', 'Category') ?>
-        ：<?= Html::a($article['category']['name'], Url::toRoute(['/category/' . $article['category']['slug']])) ?>
-    </div>
-    <div class="col-lg-3 col-sm-6">
-        <i class="glyphicon glyphicon-eye-open"></i>&nbsp;
-        <?= Yii::t('app', 'Views {total}', ['total' => $article['view']]) ?>
-    </div>
-    <div class="col-lg-3 col-sm-6">
-        <i class="glyphicon glyphicon-comment"></i>&nbsp;
-        <?php if ($article['comments_total'] > 0): ?>
-            <?= Yii::t('app', 'Comments {total}', ['total' => $article['comments_total']]) ?>
-        <?php else: ?>
+    <div id="article-info" class="row">
+        <div class="col-lg-3 col-sm-6">
+            <i class="glyphicon glyphicon-calendar"></i>&nbsp;
+            <?= Yii::t('app', 'Posted {time}', ['time' => date('Y-m-d H:i:s', $article['create_at'])]) ?>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <i class="glyphicon glyphicon-list"></i>&nbsp;
+            <?= Yii::t('app', 'Category') ?>
+            ：<?= Html::a($article['category']['name'], Url::toRoute(['/category/' . $article['category']['slug']])) ?>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <i class="glyphicon glyphicon-eye-open"></i>&nbsp;
+            <?= Yii::t('app', 'Views {total}', ['total' => $article['view']]) ?>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+            <i class="glyphicon glyphicon-comment"></i>&nbsp;
+            <!--        --><?php //if ($article['comments_total'] > 0): ?>
+            <!--            --><? //= Yii::t('app', 'Comments {total}', ['total' => $article['comments_total']]) ?>
+            <!--        --><?php //else: ?>
             <?= Yii::t('app', 'No Comments') ?>
-        <?php endif; ?>
+            <!--        --><?php //endif; ?>
+        </div>
     </div>
-</div>
 
-<hr/>
+    <hr/>
 
-<div id="article-content"><?= $article['content'] ?></div>
+    <div id="article-content"><?= $article['content'] ?></div>
+
+    <hr/>
+
+<?= $form->field($articleCommentFormModel, 'nickname')->textInput() ?>
+
+<?= $form->field($articleCommentFormModel, 'email')->textInput() ?>
+
+<?= $form->field($articleCommentFormModel, 'site')->textInput() ?>
+
+<?= $form->field($articleCommentFormModel, 'content')->textarea(['id'=>'editor','placeholder'=>'人可以走，把话留下']) ?>
+
+<?= $form->field($articleCommentFormModel, 'article_id', ['template' => '{input}'])->hiddenInput(['value' => $article['id']]) ?>
+
+<?= $form->field($articleCommentFormModel, 'reply_to', ['template' => '{input}'])->hiddenInput(['value' => '']) ?>
+
+<?= $form->field($articleCommentFormModel, 'parent_id', ['template' => '{input}'])->hiddenInput(['value' => 0]) ?>
+
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary', 'id' => 'insert-comment']) ?>
+        </div>
+    </div>
+
+<?php $form->end(); ?>
