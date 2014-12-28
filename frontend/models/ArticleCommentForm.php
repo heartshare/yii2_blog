7079250185a1419ecf3d9e80b5a1bf30b5c638cc
Mole <niefufeng@gmail.com>
2014-12-29 00:09:06 +0800

    完善评论表单模型

--- frontend/models/ArticleCommentForm.php
+++ frontend/models/ArticleCommentForm.php
@@ -3,18 +3,18 @@
 namespace frontend\models;
 
 use common\models\Article;
+use common\models\ArticleComments;
 use Yii;
 use yii\base\Model;
 
 /**
  * ArticleCommentForm is the model behind the contact form.
- * @property string $email
- * @property string $site
- * @property string $nickname
- * @property string $content
+ * @property string  $email
+ * @property string  $site
+ * @property string  $nickname
+ * @property string  $content
  * @property integer $parent_id
  * @property integer $article_id
- * @property integer $reply_to
  */
 class ArticleCommentForm extends Model
 {
@@ -25,8 +25,8 @@ class ArticleCommentForm extends Model
     public $verifyCode;
     public $parent_id;
     public $article_id;
-    public $reply_to;
     private $_article;
+    private $_replyToUserInfo;//接收回复的用户信息
 
     /**
      * @inheritdoc
@@ -37,7 +37,7 @@ class ArticleCommentForm extends Model
             // name, email, subject and body are required
             [['nickname', 'email', 'content'], 'required'],
             [['nickname', 'email', 'site'], 'trim'],
-            [['parent_id', 'article_id', 'reply_to'], 'integer'],
+            [['parent_id', 'article_id'], 'integer'],
             // email has to be a valid email address
             ['email', 'email'],
             ['site', 'url'],
@@ -67,7 +67,7 @@ class ArticleCommentForm extends Model
      * This method serves as the inline validation for article id.
      *
      * @param string $attribute the attribute currently being validated
-     * @param array $params the additional name-value pairs given in the rule
+     * @param array  $params the additional name-value pairs given in the rule
      */
     public function validateArticleID($attribute, $params)
     {
@@ -78,20 +78,49 @@ class ArticleCommentForm extends Model
         }
     }
 
+    public function addComment()
+    {
+        if ($this->validate()) {
+            $articleComments = new ArticleComments();
+            $articleComments->agent = Yii::$app->request->userAgent;
+            $articleComments->ip = Yii::$app->request->userIP;
+            $articleComments->attributes = $this->attributes;
+            if ($articleComments->save()) {
+                if ($this->parent_id) {
+                    $this->_replyToUserInfo = ArticleComments::findCommentById($this->parent_id);
+                    $this->sendEmail($this->_replyToUserInfo->email);
+                }
+                return $articleComments;
+            }
+        }
+        return null;
+    }
+
     /**
      * Sends an email to the specified email address using the information collected by this model.
      *
      * @param  string $email the target email address
+     *
      * @return boolean whether the email was sent
      */
     public function sendEmail($email)
     {
-        return Yii::$app->mailer->compose()
-            ->setTo($email)
+        return Yii::$app->mailer->compose('hasNewReply',
+            [
+                'replyToUserInfo' => $this->_replyToUserInfo,
+                'replyByUserInfo' => [
+                    'nickname' => $this->nickname,
+                    'email' => $this->email,
+                    'site' => $this->site
+                ],
+                'content' => $this->content,
+                'article' => $this->_article
+            ]
+        )->setTo($email)
             //setFrom参数的键值可以设置为发件人$this->email的邮箱地址，但是测试只有墙外的邮箱支持(比如Gmail)，国内如网易和腾讯都不支持，会报501错误，并提示不能用虚假邮箱地址。
             //如果使用国内邮箱发送，填发送邮件的邮箱地址即可。
-            ->setFrom([Yii::$app->params['notificationEmail'] => $this->nickname])
-            ->setSubject($this->subject)
+            ->setFrom([Yii::$app->params['notificationEmail'] => Yii::$app->params['sitename']])
+            ->setSubject('您在"' . Yii::$app->params['sitename'] . '"的评论有了新回复')
             ->setTextBody('Form:' . $this->email . "\n" . $this->content)
             ->send();
     }
