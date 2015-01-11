cbc27e3576f06183fbd9874bebeb977274cccf9a
聂富丰 <nieff@niefufengdeMacBook-Pro.local>
2015-01-12 01:42:20 +0800

    增加没有热门文章的判断

--- frontend/views/partial/_hotArticle.php
+++ frontend/views/partial/_hotArticle.php
@@ -1,15 +1,19 @@
 <?php
 use yii\helpers\Html;
+
 ?>
 <div class="panel panel-default hot-article">
     <div class="panel-heading">
-        <i class="glyphicon glyphicon-fire"></i>&nbsp;<?= Yii::t('app','Hot').Yii::t('app','Article') ?>
+        <i class="glyphicon glyphicon-fire"></i>&nbsp;<?= Yii::t('app', 'Hot') . Yii::t('app', 'Article') ?>
     </div>
     <ul class="list-group">
+        <?php if (!$hotArticles['list']): ?>
+            <li class="list-group-item">没有热门文章</li>
+        <?php else: ?>
         <?php foreach ($hotArticles['list'] as $article): ?>
-            <li class="list-group-item"><?= Html::a($article['title'],
-                    ['article/view', 'id' => $article['id']]) ?>
-            </li>
-        <?php endforeach; ?>
+                <li class="list-group-item"><?= Html::a($article['title'],
+                        ['article/view', 'id' => $article['id']]) ?>
+                </li>
+        <?php endforeach;endif; ?>
     </ul>
 </div>
